7148d393ac4653e557e4106d6116ec9033b240ec
Gustavo Exel <guexel@gmail.com>
2026-07-06 08:42:23 +0200

    Private-template.js

---
+++ Private-template.js
@@ -0,0 +1,4 @@
+// this is a template to create your own Private.js (Private.gs) that should have your private information
+
+const PRIVATE_Trello_APIkey = "... your Trello API key here ...";
+const PRIVATE_Trello_APItoken = "... your Trello API token here ...";
